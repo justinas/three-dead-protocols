1057894eeac1c6de8a6c15989e502b1adde66383
Justinas Stankevicius <justinas@justinas.me>
2015-07-15 23:10:22 +0300

    [qotd] Parse the port number.

--- qotd/src/main.rs
+++ qotd/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{stderr, Write};
+
 extern crate clap;
 use clap::{App, Arg};
 
@@ -6,13 +8,21 @@ fn main() {
         .author("Justinas Stankevicius")
         .arg_required_else_help(true)
         .arg(
-            Arg::with_name("quote_file").index(1)
+            Arg::with_name("quote_file").index(1).required(true)
             .help("The file with the quotes to use, one per line.")
         )
+        .arg(Arg::with_name("port").short("p").takes_value(true))
         .get_matches();
 
     // unwrap() should be fine
     // as the existence of the argument is enforced by arg_required_else_help()
     let filename = matches.value_of("quote_file").unwrap();
-    println!("{}", filename)
+    let port = matches.value_of("port").unwrap_or("17")
+        .parse::<u16>().ok().unwrap_or_else(|| {
+            write!(stderr(), "{}", "An invalid port number supplied, defaulting to 17.\n");
+            17
+        });
+
+    println!("{}", filename);
+    println!("{}", port);
 }
